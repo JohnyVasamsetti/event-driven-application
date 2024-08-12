c60030847db9e7af45d1c08638ea273e68b00fac
JohnyVasamsetti <vasamsetti.johny@gmail.com>
2024-08-12 21:07:28 +0530

    [Johny] Update lambda execution role | Create layers for lambda function

--- function/main.py
+++ function/main.py
@@ -1,6 +1,20 @@
+import json
+import boto3
+
+s3 = boto3.client('s3')
+
 def handler(event, context):
-    print("Hello from handler function !")
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+
+    response = s3.get_object(Bucket=bucket, Key=key)
+    image_data = response['Body'].read()
+
+    thumb_key = key.replace("images/", "thumbnails/")
+
+    s3.put_object(Bucket=bucket, Key=thumb_key, Body=image_data, ContentType=response['ContentType'])
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Notification sent successfully to the domain team')
+        'body': json.dumps('Image copied to thumbnails folder successfully!')
     }
--- main.tf
+++ main.tf
@@ -7,18 +7,31 @@ resource "aws_s3_bucket" "artifacts" {
 
 
 # Lambda function
+resource "null_resource" "pip_install" {
+  triggers = {
+    shell_hash = "${sha256(file("${path.module}/function/requirements.txt"))}"
+  }
+
+  provisioner "local-exec" {
+    command = "python3 -m pip install -r ./function/requirements.txt -t ${path.module}/layer/python"
+  }
+}
+
+resource "aws_lambda_layer_version" "layer" {
+  layer_name = "python-dependencies"
+  filename = data.archive_file.dependency_archive.output_path
+  source_code_hash = data.archive_file.dependency_archive.output_base64sha256
+  compatible_runtimes = ["python3.9", "python3.10"]
+}
+
 resource "aws_lambda_function" "event_handler" {
   function_name = "s3-event-handler"
   role          = aws_iam_role.lambda_exec.arn
-  filename      = archive_file.lambda_zip.output_path
+  filename      = data.archive_file.code_archive.output_path
+  source_code_hash = data.archive_file.code_archive.output_base64sha256
   handler       = "main.handler"
-  runtime       = "python3.8"
-}
-
-resource "archive_file" "lambda_zip" {
-  output_path = "lambda_function"
-  source_dir = "./function"
-  type        = "zip"
+  runtime       = "python3.10"
+  layers        = [aws_lambda_layer_version.layer.arn]
 }
 
 
@@ -28,6 +41,8 @@ resource "aws_s3_bucket_notification" "s3_event_notifications" {
   lambda_function {
     lambda_function_arn = aws_lambda_function.event_handler.arn
     events = ["s3:ObjectCreated:*"]
+    filter_prefix = "images/"
+    filter_suffix = ".jpg"
   }
 }
 
@@ -49,8 +64,34 @@ resource "aws_iam_role" "lambda_exec" {
   })
 }
 
+resource "aws_iam_policy" "lambda_exec_policy" {
+  name        = "lambda-exec-policy"
+  path        = "/"
+  description = "Permissions for lambda function to execute code"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "S3ObjectGetPermissions"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ]
+        Effect   = "Allow"
+        Resource = "${aws_s3_bucket.artifacts.arn}/*"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role_policy_attachment" "lambda_exec_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.lambda_exec.name
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_exec_policy-2" {
+  policy_arn = aws_iam_policy.lambda_exec_policy.arn
   role       = aws_iam_role.lambda_exec.name
 }
 
